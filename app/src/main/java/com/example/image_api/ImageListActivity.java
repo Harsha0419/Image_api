afd724b4623a2c54888300d81dfa8babf67c54ca
[Harsha0419] <[palagiriganeshharsha143@gmail.com]>
2023-06-02 11:05:47 +0530

    second Commit

--- app/src/main/java/com/example/image_api/ImageListActivity.java
+++ app/src/main/java/com/example/image_api/ImageListActivity.java
@@ -1,10 +1,12 @@
 package com.example.image_api;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,11 +22,13 @@ public class ImageListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_list);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
         adapter = new ImageAdapter();
         recyclerView.setAdapter(adapter);
+
 
 
         ViewModelProvider.Factory factory = new ViewModelFactory();
--- app/src/main/java/com/example/image_api/MainActivity.java
+++ app/src/main/java/com/example/image_api/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.image_api;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnViewImages;
+    private CardView btnViewImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         btnViewImages = findViewById(R.id.btnViewImages);
         btnViewImages.setOnClickListener(new View.OnClickListener() {
